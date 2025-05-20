306a7af58cca62f2bf1a9a24ae02c6411afa0e1b
qkrxogmla <suk05553@naver.com>
2025-05-20 15:54:37 +0900

    header완성

--- vite-tailwind-template/src/App.jsx
+++ vite-tailwind-template/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import BottomBar from "./components/BottomBar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <BottomBar />
     </div>
   );
---
+++ vite-tailwind-template/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const pathname = window.location.pathname;
+  const navigate = useNavigate();
+
+  const showBackButton = !(
+    pathname.includes("home") ||
+    pathname.includes("report") ||
+    pathname.includes("mypage")
+  );
+
+  const renderCenterContent = () => {
+    if (pathname.includes("routine/add")) return "루틴추가하기";
+    if (pathname.includes("home"))
+      return <img src="/assets/logo.svg" alt="logo" className="h-6" />;
+    if (pathname.includes("report")) return "월별 리포트";
+    if (pathname.includes("mypage")) return "마이페이지";
+    if (pathname.includes("lookback")) return "되돌아보기";
+    return "";
+  };
+
+  return (
+    <div className="fixed top-0 w-full flex justify-between items-center bg-white pt-[20px] pb-[12px] px-[20px] z-50">
+      <div className="w-6 h-6">
+        {showBackButton ? (
+          <img
+            src="/assets/backbutton.svg"
+            alt="back"
+            className="w-full h-full cursor-pointer"
+            onClick={() => navigate(-1)}
+          />
+        ) : null}
+      </div>
+      <div className="text-base font-semibold">{renderCenterContent()}</div>
+      <div className="w-6 h-6"></div>
+    </div>
+  );
+};
+
+export default Header;
